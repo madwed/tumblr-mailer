c1055594768c02d393501796a5d1732a4c1e520c
Isaac Madwed <i.madwed@gmail.com>
2015-06-06 15:46:25 -0400

    updated tumblr_mailer.js so it will send an email to each of my contacts with blog updates

--- tumblr_mailer.js
+++ tumblr_mailer.js
@@ -3,7 +3,7 @@ var ejs = require("ejs");
 var tumblr = require("tumblr.js");
 var mandrill = require("mandrill-api/mandrill");
 
-var mandrillKey = fs.readFileSync("mandrill_key.txt");
+var mandrillKey = fs.readFileSync("mandrill_key.txt") + "";
 
 var mandrill_client = new mandrill.Mandrill(mandrillKey);
 
@@ -44,27 +44,6 @@ var emailTemplate = fs.readFileSync("email_template.ejs", "utf8");
 var tumblrKey = fs.readFileSync("tumblr_key.txt", "utf8");
 tumblrKey = JSON.parse(tumblrKey);
 
-var client = tumblr.createClient(tumblrKey);
-var latestPosts = client.posts("codefishandchips.tumblr.com", function(err, blog){
-	if(err){ return console.log(err); }
-	var oneWeekAgo = Date.now() - (7 * 24 * 60 * 60 * 1000);
-	var latestPosts = blog.posts.filter(function(post){
-		var postDate = Date.parse(post.date);
-		if(postDate > oneWeekAgo){
-			return true;
-		}
-		return false;
-	});
-
-	var csv_data = csvParse(csvFile);
-
-	csv_data.forEach(function(row){
-		row.latestPosts = latestPosts;
-		var templateCopy = ejs.render(emailTemplate, row);
-		console.log(templateCopy);
-	});
-});
-
 function csvParse(csvFile){
 	var contacts = csvFile.split("\n");
 	contacts.splice(0, 1);
@@ -84,3 +63,26 @@ function csvParse(csvFile){
 	}
 	return contacts;
 }
+
+var client = tumblr.createClient(tumblrKey);
+var latestPosts = client.posts("codefishandchips.tumblr.com", function(err, blog){
+	if(err){ return console.log(err); }
+	var oneWeekAgo = Date.now() - (7 * 24 * 60 * 60 * 1000);
+	var latestPosts = blog.posts.filter(function(post){
+		var postDate = Date.parse(post.date);
+		if(postDate > oneWeekAgo){
+			return true;
+		}
+		return false;
+	});
+
+	var csv_data = csvParse(csvFile);
+
+	csv_data.forEach(function(row){
+		row.latestPosts = latestPosts;
+		var templateCopy = ejs.render(emailTemplate, row);
+		sendEmail(row.firstName, row.emailAddress, "Isaac", "i.madwed@gmail.com", "Check out my blog", templateCopy);
+	});
+});
+
+
